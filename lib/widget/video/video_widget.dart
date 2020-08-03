1881089be7462170f983be1f03225b60d718b140
aivuca <67494078+aivuca@users.noreply.github.com>
2020-08-03 19:34:53 +0800

    fix typo
    fix typo

--- lib/widget/video/video_widget.dart
+++ lib/widget/video/video_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
- import 'package:flutter_hrlweibo/constant/constant.dart';
+import 'package:flutter_hrlweibo/constant/constant.dart';
 import 'package:video_player/video_player.dart';
 
 
